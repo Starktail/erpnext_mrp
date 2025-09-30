1e1f52cde862605709683c9de7033615e6d25c71
Dirk van der Laarse <dirk@laarse.co.za>
2025-09-30 10:34:55 +0000

    fix: csrf issue

--- erpnext_mrp/www/erpnext_mrp.py
+++ erpnext_mrp/www/erpnext_mrp.py
@@ -4,9 +4,8 @@ from frappe.utils.telemetry import capture
 no_cache = 1
 
 
-def get_context():
+def get_context(context):
 	csrf_token = frappe.sessions.get_csrf_token()
 	frappe.db.commit()
-	context = frappe._dict()
-	context.boot.csrf_token = csrf_token
+	context.csrf_token = csrf_token
 	return context
