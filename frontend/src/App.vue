984fce5850eae7c2ebf1e2579562723a47444275
Dirk van der Laarse <dirk@laarse.co.za>
2025-10-20 11:54:04 +0000

    fix: double scrollbar in ui

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex h-screen bg-gray-100">
     <AppSidebar />
-    <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
-      <div class="p-6">
+    <main class="flex-1 overflow-x-hidden overflow-hidden bg-gray-200">
+      <div class="p-6 h-full">
         <router-view />
       </div>
     </main>
